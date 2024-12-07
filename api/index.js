c4edbe35d1387c312eac221cb83916fc7837eb49
israthnr <israthnr.20@uom.lk>
2024-12-07 09:04:06 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import useRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js"
 dotenv.config();
 
 mongoose
@@ -14,6 +16,12 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+
+//path: api/index.js
+app.use('/api/user', useRouter);
+app.use('/api/auth', authRouter);
